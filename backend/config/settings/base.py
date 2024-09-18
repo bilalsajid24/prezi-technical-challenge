748276d39956d01ef4cb98dde86a293634e65965
Bilal Sajid <bilal.sajid@sprk.global>
2024-09-18 19:58:20 +0200

    chore: add django application for presentations

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -45,7 +45,9 @@ THIRD_PARTY_APPS = [
     "rest_framework",
 ]
 
-LOCAL_APPS = []
+LOCAL_APPS = [
+    "server.presentations.apps.PresentationsConfig",
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 from .base import *
 from .base import env
 
@@ -12,3 +15,47 @@ ADMIN_URL = env("DJANGO_ADMIN_URL")
 
 # LOGGING
 REQUEST_LOGGING_ENABLE_COLORIZE = False
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(pathname)s %(funcName)s %(message)s"
+        },
+        "simple": {
+            "format": "%(levelname)s %(asctime)s %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": logging.INFO,
+            "class": "logging.StreamHandler",
+            "stream": sys.stderr,
+            "formatter": "verbose"
+        },
+        "console_simple": {
+            "level": logging.INFO,
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "simple",
+        }
+    },
+    "loggers": {
+        "django.db.backends": {
+            "level": logging.ERROR,
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "django.security.DisallowedHost": {
+            "level": logging.ERROR,
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "django.request": {
+            "handlers": ["console_simple"],
+            "level": logging.INFO,
+            "propagate": False
+        },
+    },
+}
+
